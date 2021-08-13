8e0d9321a5ffd0074a0e2180088d051cadefe708
Icaro Florencio <icaro.florencio@mercadolivre.com>
2021-08-12 22:07:34 -0300

    Los primeros pasos con JDBC

---
+++ src/main/java/com/alura/jdbc/PruebaConexion.java
@@ -0,0 +1,20 @@
+package com.alura.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class PruebaConexion {
+
+    public static void main(String[] args) throws SQLException {
+        Connection con = DriverManager.getConnection(
+                "jdbc:mysql://localhost/control_de_stock?useTimeZone=true&serverTimeZone=UTC",
+                "root",
+                "root1234");
+
+        System.out.println("Cerrando la conexión");
+
+        con.close();
+    }
+
+}
